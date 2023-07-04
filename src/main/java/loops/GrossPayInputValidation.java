22a6b47e78a99fcb1c83113283a2ad8f44e54dc4
Gareth Macdonald <macdonald.gareth@gmail.com>
2023-07-04 07:26:57 +0200

    Basic while loop implemented

---
+++ src/main/java/loops/GrossPayInputValidation.java
@@ -0,0 +1,34 @@
+/*
+ * GrossPayInputValidation
+ * v1.0
+ * 2023-07-4
+ */
+package loops;
+
+import java.util.Scanner;
+
+/**
+ * Allow employees to enter their hours worked per week.
+ * NOTE: Does not allow for overtime to be entered
+ *
+ * @author Gareth Macdonald
+ */
+
+public class GrossPayInputValidation {
+    public static void main(String[] args) {
+        double rate = 15;
+        double maxHours = 40;
+        double minHours = 1;
+
+        System.out.println("How many hours did you work?");
+        Scanner scanner = new Scanner(System.in);
+        double hoursWorked = scanner.nextDouble();
+
+        while (hoursWorked > maxHours || hoursWorked < minHours){
+            System.out.println("Invalid input, please enter a number between and including 1 and 40");
+            hoursWorked = scanner.nextDouble();
+        }
+        double weeklyPay = rate * hoursWorked;
+        System.out.println("Weekly gross pay is: $" + weeklyPay);
+    }
+}
